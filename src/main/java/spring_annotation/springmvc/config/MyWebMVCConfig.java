08e39e93ba9ed75b22f26ac08b3cca725eab6a95
TongAJ <875334300@qq.com>
2018-07-30 17:44:52 +0800

    Spring源码解读txt以及SpringMvc利用servlet3.0机制，整合SpringMVC和利用servlet3.0机制如何处理异步请求

---
+++ src/main/java/spring_annotation/springmvc/config/MyWebMVCConfig.java
@@ -0,0 +1,36 @@
+package spring_annotation.springmvc.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * 启动WEB时，会自动创建对象，调用方法来初始化对象及前端控制器
+ */
+public class MyWebMVCConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    /**
+     * 获取根容器的配置类--父容器（SpringConfig）
+     * @return
+     */
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{RootConfig.class};
+    }
+
+    /**
+     * SpringWeb容器--子容器
+     * @return
+     */
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{ServletConfig.class};
+    }
+
+    /**
+     * 获取DispatchServlet的前端控制器拦截信息
+     * @return
+     */
+    protected String[] getServletMappings() {
+        //拦截所有请求和静态资源 但不包括/.jsp页面
+        //所有不能用/*,这样不会拦截jsp
+        return new String[]{"/"};
+    }
+
+}
